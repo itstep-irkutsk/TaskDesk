af9f78973055d1e55af6f45f77980435fae5e5f2
Никита <gl00m@inbox.ru>
2021-09-28 18:35:19 +0800

    Создание базы данных

---
+++ TaskDeskDB/script.sql
@@ -0,0 +1,25 @@
+create table table_priority
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    priority TEXT NOT NULL
+);
+
+create table table_status
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    status TEXT NOT NULL
+);
+
+create table table_task
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name           TEXT NOT NULL,
+    description    TEXT,
+    creation_date  TEXT NOT NULL,
+    execution_date TEXT NOT NULL,
+    status_id      INTEGER NOT NULL,
+    priority_id    INTEGER NOT NULL,
+    hidden         INTEGER NOT NULL
+);
+
+
