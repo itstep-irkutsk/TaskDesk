9f1c1c6fb1bfded260d5f712981e83a9cb59296b
Ivan Sh <irkrus3824@mail.ru>
2021-09-28 19:14:06 +0800

    Сделал прототип базового класса

---
+++ main_body/main_body_class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace main_body
+{
+    public abstract class Note
+    {
+        public DateTime _opendatetime;
+        public DateTime _closedatetime;
+        public string _name;
+        public string _notice;
+        public bool _status;
+    }
+    
+    
+    
+}
