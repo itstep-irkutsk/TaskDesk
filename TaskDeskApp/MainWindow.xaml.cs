4813f5576b27f48014486153b08962af3946512b
Roman Sitnikov <romzes2006@gmail.com>
2021-10-01 14:26:39 +0800

    Пишем динамическое заполнение календаря

--- TaskDeskApp/MainWindow.xaml.cs
+++ TaskDeskApp/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Xml.Linq;
 
 namespace TaskDeskApp
 {
@@ -10,12 +13,44 @@ namespace TaskDeskApp
             InitializeComponent();
         }
 
-   
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-           MessageBox.Show("Работает");
-           Event2.Items.Add(new ListBoxItem().Content="Задача 2");
+            MessageBox.Show("Работает");
+            PushListBoxIntoGrid(1, 1, Calendar);
+            //Event2.Items.Add(new ListBoxItem().Content="Задача 2");
+        }
+
+        private void PushListBoxIntoGrid(int row, int column, Grid gridname)
+        {
+            var listBox = new ListBox
+            {
+                //Foreground = new Brushes(BlurEffect.RadiusProperty.);
+                Margin = new Thickness(2),
+                Padding = new Thickness(1),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top
+            };
+            var listboxitem = new ListBoxItem().Content = "Событие 1";
+            listBox.Items.Add(listboxitem);
+
+            RemoveChildElementFromGrid(row,column,gridname);
+            Grid.SetRow(listBox, row);
+            Grid.SetColumn(listBox, column);
+
+            gridname.Children.Add(listBox);
+        }
+
+        private void RemoveChildElementFromGrid(int row, int column, Grid gridname)
+        {
+            for (int i = 0; i < gridname.Children.Count; i++)
+            {
+                if ((Grid.GetRow(gridname.Children[i]) == row) && ((Grid.GetColumn(gridname.Children[i])) == column))
+                {
+                    gridname.Children.Remove(gridname.Children[i]);
+                    break;
+                }
+            }
         }
     }
 }
